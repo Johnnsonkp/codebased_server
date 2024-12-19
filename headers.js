955962446dac886d233ca8653371cf9a2a2377d7
John Nkp <john.nkp1@gmail.com>
2024-12-20 04:07:37 +1100

    ammended env variables

--- headers.js
+++ headers.js
@@ -1,8 +1,8 @@
-import config from "./config.cjs"
+// import config from "./config.cjs"
 
 export const headerOptions = {
   "Access-Control-Allow-Origin": "*",
-  Authorization: `Bearer ${config.githubToken}`,
+  Authorization: `Bearer ${process.env.VITE_APP_GITHUB_TOKEN}`,
   "Accept": "application/vnd.github.raw+json",
   "content-type": "text/plain",
   'X-Github-Api-Version': '2022-11-28'
@@ -10,7 +10,7 @@ export const headerOptions = {
 
 export const headerOptions2 = {
   "Access-Control-Allow-Origin": "*",
-  Authorization: `Bearer ${config.githubToken}`,
+  Authorization: `Bearer ${process.env.VITE_APP_GITHUB_TOKEN}`,
   "Accept": "application/vnd.github.raw+json",
   "content-Type": "application/json",
   'X-Github-Api-Version': '2022-11-28'
@@ -18,7 +18,7 @@ export const headerOptions2 = {
 
 export const headerOptions3 = {
   "Access-Control-Allow-Origin": "*",
-  Authorization: `Bearer ${config.githubToken}`,
+  Authorization: `Bearer ${process.env.VITE_APP_GITHUB_TOKEN}`,
   "Accept": "application/vnd.github.raw+json",
   "content-Type": "application/json",
   'X-Github-Api-Version': '2022-11-28'
